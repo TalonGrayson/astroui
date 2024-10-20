417d08f636958dbeacd8de0ac2d786d279f769d8
Talon Grayson <talongrayson@gmail.com>
2024-10-20 18:31:12 +0100

    Add Prisma, database schema, migration

---
+++ prisma/migrations/20241020163023_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Scannable" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "scannable_id" INTEGER NOT NULL,
+    "category" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "action" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Scannable_scannable_id_key" ON "Scannable"("scannable_id");
